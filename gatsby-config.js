e387c471eaf2eb566e81a39facc568278f2e5220
Ryota Hirano <hirano@tsumikiinc.com>
2018-12-29 00:40:07 +0900

    add markdown blog content use graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  plugins: [`gatsby-plugin-emotion`]
+  plugins: [
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src`,
+        name: 'src'
+      }
+    },
+    `gatsby-transformer-remark`
+  ]
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,56 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import { css } from '@emotion/core';
 import Header from '../components/Header';
 
-export default () => (
-  <div
-    css={css`
-      display: flex;
-      justify-content: center;
-    `}>
-    <Header title="Gatsby Blog" />
-  </div>
-);
+export default ({ data }) => {
+  return (
+    <div>
+      <Header title="Gatsby Blog" />
+      <div
+        css={css`
+          width: 600px;
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
+          margin: 0 auto;
+          padding-top: 40px;
+        `}>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div
+            key={node.id}
+            css={css`
+              width: 100%;
+              & + & {
+                margin-top: 40px;
+              }
+            `}>
+            <h2
+              css={css`
+                margin: 0;
+              `}>
+              {node.frontmatter.title}
+            </h2>
+            <span>{node.frontmatter.date}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date
+          }
+        }
+      }
+    }
+  }
+`;
